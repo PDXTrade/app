57b412900ca89ef87a82a3e5ebcbc8ed86c60789
G.Provost <grace.g.provost@gmail.com>
2018-02-05 11:00:00 -0800

    work in header

---
+++ src/components/services/firebase.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBe_-OqBKBHBBSv3vOt9_N9pJVKBtvAA20",
+  authDomain: "tradepdx-6f568.firebaseapp.com",
+  databaseURL: "https://tradepdx-6f568.firebaseio.com",
+  projectId: "tradepdx-6f568",
+  storageBucket: "",
+  messagingSenderId: "250899403871"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export const db = firebaseApp.database(); //the real-time database
+export const storage = firebase.storage(); //the firebase storage adjunct for images
+export const auth = firebaseApp.auth(); //the firebase auth namespace
+
+
+// auth.onAuthStateChanged(user => {
+//   if (user) {
+//     user.getIdToken().then(token => {
+//       store.dispatch(userFromToken(token));
+//     });
+//   }
+//     store.dispatch({ type: LOGOUT });
+//   }
+//   else {
+//     store.dispatch({ type: TOKEN_CHECK });
+//   }
+// });
