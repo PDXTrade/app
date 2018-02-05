56559e64a6fee864288bfb203c1cd83b135a59c5
G.Provost <grace.g.provost@gmail.com>
2018-02-05 13:07:55 -0800

    create user login functionality

--- src/components/app/header/Header.js
+++ src/components/app/header/Header.js
@@ -12,18 +12,22 @@ export default class Header {
   render() {
     const dom = template.clone();
     const userItem = dom.querySelector('.user-nav');
+    const tradesLink = dom.querySelector('.trades-link');
 
     auth.onAuthStateChanged(user => {
       let child = null;
 
       if(user) {
         child = new User().render();
+        tradesLink.classList.remove('hidden');
       }
       else {
         child = document.createElement('a');
-        child.textContent = 'Sign In';
-        child.href = '#auth';
+        child.innerHTML = '<span class="fa fa-user"></span> Login';
+        child.href = '#login';
         userItem.appendChild(child);
+        tradesLink.classList.add('hidden');
+        
       }
 
       removeChildren(userItem);
