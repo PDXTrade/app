9c322e358f4fd997d3db8009b86b3691e3de916f
G.Provost <grace.g.provost@gmail.com>
2018-02-05 17:53:07 -0800

    fiddle with category nav

--- src/components/app/header/Categories.js
+++ src/components/app/header/Categories.js
@@ -1,20 +1,12 @@
 import html from './categories.html';
 import Template from '../../Template';
-// import { db } from '../../../services/firebase';
 
 const template = new Template(html);
-// const categoriesDB = db.ref('categories');
 
 export default class User {
 
   render() {
     const dom = template.clone();
-    const categories = dom.querySelector('ul');
-
-    categories.addEventListener('click', (event) => {
-      event.preventDefault();
-      window.location.hash = `#home/${event.target.textContent}`;
-    });
 
     return dom;
   }
