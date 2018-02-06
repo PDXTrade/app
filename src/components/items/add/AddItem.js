7c21833f2b42e3774cf5f5c4b298477435513207
G.Provost <grace.g.provost@gmail.com>
2018-02-06 10:21:07 -0800

    building items functionality. Start add new item functionality.

--- src/components/items/add/AddItem.js
+++ src/components/items/add/AddItem.js
@@ -1,9 +1,10 @@
 import Template from '../../Template';
 import html from './add-item.html';
 import './add-item.css';
-
+import { db } from '../../../services/firebase';
 
 const template = new Template(html);
+const items = db.ref('items');
 
 export default class AddItem {
   constructor(onAdd) {
