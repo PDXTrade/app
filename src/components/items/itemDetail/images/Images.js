934f33ea7afabcb4ec3a45811a69d147779e0eaf
G.Provost <grace.g.provost@gmail.com>
2018-02-06 18:48:33 -0800

    start images functionality

--- src/components/items/itemDetail/images/Images.js
+++ src/components/items/itemDetail/images/Images.js
@@ -1,16 +1,17 @@
-import Template from '../../Template';
+import Template from '../../../Template';
 import html from './images.html';
 import Image from './Image';
-import { db, storage } from '../../../services/firebase';
+import { db, storage } from '../../../../services/firebase';
 
 const template = new Template(html);
-const itemsImages = db.ref('items-images');
+const itemImages = db.ref('itemImages');
 const itemImageStorage = storage.ref('items');
 
 export default class Images {
-  constructor(key) {
-    this.itemsImages = itemsImages.child(key);
+  constructor(key, editable) {
+    this.itemsImages = itemImages.child(key);
     this.imageStorage = itemImageStorage.child(key);
+    this.editable = editable;
   }
 
   handleUpload(file) {
@@ -27,3 +28,62 @@ export default class Images {
       itemImage.set(downloadUrl);
     });
   }
+
+  handleRemove(imageKey) {
+    this.petImages.child(imageKey).remove();
+    const storage = this.imageStorage.child(imageKey);
+    storage.delete()
+      .catch(err => {
+        if(err.code === 'storage/object-not-found') return;
+        console.error(err);
+      });
+  }
+
+  render() {
+    const dom = template.clone();
+    
+    if(this.editable) {
+      this.fileInput = dom.querySelector('input[type=file]');
+      this.fileInput.addEventListener('change', event => {
+        const files = event.target.files;
+        if(!files || !files.length) return;
+        this.handleUpload(files[0]);
+      });
+
+      const embedForm = dom.querySelector('form');
+      embedForm.addEventListener('submit', event => {
+        event.preventDefault();
+        this.handleEmbed(event.target.elements.url.value);
+      });
+    }
+    else {
+      dom.querySelector('.upload').remove();
+    }
+
+    const ul = dom.querySelector('ul');
+    const map = new Map();
+
+    this.childAdded = this.petImages.on('child_added', data => {
+      const image = new Image(data.val(), this.editable ? () => this.handleRemove(data.key) : null);
+      const imageDom = image.render();
+      map.set(data.key, {
+        nodes: [...imageDom.childNodes],
+        component: image
+      });
+      ul.appendChild(imageDom);
+    });
+
+    this.childRemoved = this.petImages.on('child_removed', data => {
+      const toRemove = map.get(data.key);
+      toRemove.nodes.forEach(node => node.remove());
+      // toRemove.component.unrender();
+    });
+
+    return dom;
+  }
+
+  unrender() {
+    this.petImages.on('child_added', this.childAdded);
+    this.petImages.on('child_removed', this.childRemoved);
+  }
+}
