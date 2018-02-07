fc61495aa1d9df4666b0fcd9524e3a66562f80fc
mewarren <holla@mewarren.com>
2018-02-07 15:04:56 -0800

    fixed userPage url

--- src/components/user/UserPage.js
+++ src/components/user/UserPage.js
@@ -7,18 +7,18 @@ import { db } from '../../services/firebase';
 
 const template = new Template(html);
 
-const key = window.location.hash.split('/')[1];
-const userItems = db.ref('itemsByUser').child(key);
 
 export default class UserPage {
   
   render() { 
-
+    
     const dom = template.clone();
     
     this.header = dom.querySelector('#username-header');
     this.section = dom.querySelector('#user-item-list');    
     
+    const key = window.location.hash.split('/')[1];
+    const userItems = db.ref('itemsByUser').child(key);
     const userList = new ItemList(userItems).render();
     this.section.append(userList);
     
