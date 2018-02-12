8b5f8a8c4ff3bf4673b071c0ec92805ed4862050
G.Provost <grace.g.provost@gmail.com>
2018-02-11 16:19:37 -0800

    add trade status functionality to tradesView page

--- src/components/tradesView/TradesViewList.js
+++ src/components/tradesView/TradesViewList.js
@@ -1,6 +1,6 @@
 import Template from '../Template';
 import html from './trades-view-list.html';
-import { db } from '../../services/firebase';
+import { db, auth } from '../../services/firebase';
 
 const template = new Template(html);
 const trades = db.ref('trades');
@@ -19,6 +19,7 @@ export default class TradesViewList {
     const myName = dom.querySelector('span.my-name');
     const theirName = dom.querySelector('span.their-name');
     const aTag = dom.querySelector('a');
+    const status = dom.querySelector('.status');
 
     aTag.href = `#trade/${this.tradeKey}`;
 
@@ -26,6 +27,15 @@ export default class TradesViewList {
       const trade = data.val();
       myName.textContent = trade.user2Name;
       theirName.textContent = trade.user1Name;
+
+      if(trade.status) {
+        if(auth.currentUser.uid === trade.sentBy && (trade.status === 'new offer' || trade.status === 'counter offer')) status.textContent = 'pending';
+        else if(auth.currentUser.uid !== trade.sentBy && trade.status === 'new offer') status.textContent = 'new offer';
+        else if(auth.currentUser.uid !== trade.sentBy && trade.status === 'counter offer') status.textContent = 'new counter offer';
+        else if(trade.status === 'accepted' || trade.status === 'rejected') status.textContent = trade.status;
+      } else {
+        status.textContent = 'new trade pending';
+      }
     });
 
     return dom;
